95705cd2b1b230446655c09965e5efcf34d17aff
Yoshiya Hinosawa <stibium121@gmail.com>
2019-12-27 01:04:08 +0900

    feat: --name options

--- README.md
+++ README.md
@@ -12,16 +12,12 @@ steps:
   - uses: denolib/setup-deno@v1.1.0
     with:
       deno-version: 0.x
-  - run: <your command> | deno https://raw.githubcontent.com/.../main.ts
+  - run: <your command> | deno https://raw.githubusercontent.com/kt3k/set_output/master/main.ts --name myparam
     id: mystep
-  - run: echo ${{ steps.mystep.output.value }}
+  - run: echo ${{ steps.mystep.output.myparam }}
 ```
 
-In the above example, the output of `<your command>` is stored in `steps.mystep.output.value` and you can use it in later steps.
-
-# TODO
-
-- [ ] make the name of the output configurable
+In the above example, the output of `<your command>` is stored in `steps.mystep.output.myparam` and you can use it in later steps.
 
 # License
 
--- main.ts
+++ main.ts
@@ -1,9 +1,28 @@
+import { parse } from 'https://deno.land/std/flags/mod.ts'
+
+const { help, name } = parse(Deno.args, {
+  string: ['name'],
+  boolean: ['help'],
+  alias: {
+    h: 'help'
+  }
+})
+
+if (help) {
+  console.log(`Usage: set_output [--name <name>]
+
+Example:
+  <your command> | deno set_output --name myparam
+
+  This sets the output of your command to the output of the step in GitHub Actions.`)
+  Deno.exit(0)
+}
+
 const decoder = new TextDecoder()
 const input = decoder.decode(await Deno.readAll(Deno.stdin))
 
 // ref: https://github.com/actions/toolkit/blob/e8d384d3afbb1f6ab7f173155ae0404242a34d80/packages/core/src/command.ts#L78
 const escaped = input.replace(/\r/g, '%0D').replace(/\n/g, '%0A')
 
-// TODO: value from --name value
 const output = `::set-output name=value::${escaped}`
 console.log(output)
