b2941dc44517a0f343ad1a6185e967c20dbba0e2
Yoshiya Hinosawa <stibium121@gmail.com>
2020-01-08 18:15:24 +0900

    feat: trim input

--- main.ts
+++ main.ts
@@ -28,7 +28,7 @@ const decoder = new TextDecoder()
 const input = decoder.decode(await Deno.readAll(Deno.stdin))
 
 // ref: https://github.com/actions/toolkit/blob/e8d384d3afbb1f6ab7f173155ae0404242a34d80/packages/core/src/command.ts#L78
-const escaped = input.replace(/\r/g, '%0D').replace(/\n/g, '%0A')
+const escaped = input.trim().replace(/\r/g, '%0D').replace(/\n/g, '%0A')
 
 const output = `::set-output name=value::${escaped}`
 console.log(output)
